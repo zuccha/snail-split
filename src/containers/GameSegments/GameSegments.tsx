76d08549224be090cffb61e1c1ac7e9d17256db6
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-02 14:04:17 +0200

    Create GameSegments empty container component

---
+++ src/containers/GameSegments/GameSegments.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BoxProps } from 'ink'
+import Segments from '../../components/Splits'
+
+
+interface IGameSegmentsProps {
+  containerProps?: BoxProps
+}
+
+
+const GameSegments: React.FC<IGameSegmentsProps> = ({
+  containerProps = undefined,
+}) => {
+  return null
+}
+
+
+export default GameSegments
