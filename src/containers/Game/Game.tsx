098b0e24939c87f3776d1c5f966e3d69f5861c17
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-02 15:28:45 +0200

    Start clock when program starts

--- src/containers/Game/Game.tsx
+++ src/containers/Game/Game.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Box, useStdout } from 'ink'
+import createActionGameStart from '../../store/game/actions/createActionGameStart'
+import createActionGameTick from '../../store/game/actions/createActionGameTick'
 import GameHeader from '../GameHeader'
 import GameSegments from '../GameSegments'
 import GameTime from '../GameTime'
 
 
+const TICK_INTERVAL = 32
+
 const Game: React.FC<{}> = ({
 
 }) => {
   const { stdout } = useStdout()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(createActionGameStart())
+    setInterval(() => {
+      dispatch(createActionGameTick())
+    }, TICK_INTERVAL)
+  }, [])
 
   return (
     <Box
