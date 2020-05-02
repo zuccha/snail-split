849a95d8788ccbf02342dbe348629a4cde292c1d
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-02 14:04:33 +0200

    Create GameTime empty container component

---
+++ src/containers/GameTime/GameTime.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BoxProps } from 'ink'
+import Time from '../../components/Time'
+
+
+interface IGameTimeProps {
+  containerProps?: BoxProps
+}
+
+
+const GameTime: React.FC<IGameTimeProps> = ({
+  containerProps = undefined,
+}) => {
+  return null
+}
+
+
+export default GameTime
