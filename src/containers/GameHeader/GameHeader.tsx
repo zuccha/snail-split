825d25c5bca961ee94366025395e4209a9ef70e0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-02 14:03:46 +0200

    Create GameHeader container component

---
+++ src/containers/GameHeader/GameHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BoxProps } from 'ink'
+import Header from '../../components/Header'
+import selectTitle from '../../store/game/selectors/selectTitle'
+
+
+interface IGameHeaderProps {
+  containerProps?: BoxProps
+}
+
+
+const GameHeader: React.FC<IGameHeaderProps> = ({
+  containerProps = undefined,
+}) => {
+  const title = useSelector(selectTitle)
+
+  return (
+    <Header title={title} containerProps={containerProps} />
+  )
+}
+
+
+export default GameHeader
