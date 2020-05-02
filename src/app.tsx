b98827fa674e83cce597ef4475eaeeb8a3bf0f3b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-02 14:06:05 +0200

    Connect App to store

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Text, Color } from 'ink'
+import React from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
+import Game from './containers/Game'
+import store from './store'
 
 
-interface AppProps {
-  name: string
+interface IAppProps {
+
 }
 
-const App: React.FC<AppProps> = ({
-  name = 'Stranger',
-}) => {
-  const [time, setTime] = useState(0);
-  
-  useEffect(() => {
-    setInterval(() => setTime(prevTime => prevTime + 1), 1000)
-  }, []);
 
+const App: React.FC<IAppProps> = () => {
   return (
-    <Text>
-      Hello, <Color green>{name}</Color>, {time} seconds have passed.
-    </Text>
+    <ReduxProvider store={store}>
+      <Game />
+    </ReduxProvider>
   )
 }
 
