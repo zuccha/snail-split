c91a6cf11ac4f39650cb17dad9be79a186f3d0f0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-05-10 17:45:21 +0200

    Reimplement save

--- src/containers/Game/useInputs.ts
+++ src/containers/Game/useInputs.ts
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
+import fs from 'fs'
 import screen from '../../screen'
 import createActionGameReset from '../../store/game/actions/createActionGameReset'
 import createActionGameSplit from '../../store/game/actions/createActionGameSplit'
 import createActionGameToggle from '../../store/game/actions/createActionGameToggle'
+import store from '../../store'
 import useDispatch from '../../store/useDispatch'
 
 
@@ -10,18 +12,39 @@ const useInputs = (filename: string): void => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const exit = (): void => { process.exit(0) }
-    const toggleGame = (): void => { dispatch(createActionGameToggle()) }
-    const resetGame = (): void => { dispatch(createActionGameReset()) }
-    // const saveGame = (): void => { dispatch(createActionGameSave(filename)) }
-    const splitGame = (): void => { dispatch(createActionGameSplit()) }
+    const exit = (): void => {
+      process.exit(0)
+    }
+
+    const toggleGame = (): void => {
+      dispatch(createActionGameToggle())
+    }
+
+    const resetGame = (): void => {
+      dispatch(createActionGameReset())
+    }
+
+    const saveGame = (): void => {
+      try {
+        const game = store.getState().game
+        const gameJson = JSON.stringify(game, null, 2)
+        fs.writeFileSync(filename, gameJson)
+        // TODO: Notify success.
+      } catch (e) {
+        // TODO: Notify failure.
+      }
+    }
+
+    const splitGame = (): void => {
+      dispatch(createActionGameSplit())
+    }
 
     screen.key('escape', exit)
     screen.key('q', exit)
     screen.key('C-c', exit)
     screen.key('space', toggleGame)
     screen.key('r', resetGame)
-    // screen.key('s', saveGame)
+    screen.key('s', saveGame)
     screen.key('return', splitGame)
 
     return () => {
@@ -29,11 +52,11 @@ const useInputs = (filename: string): void => {
       screen.unkey('q', exit)
       screen.unkey('C-c', exit)
       screen.unkey('space', toggleGame)
-      // screen.unkey('s', saveGame)
+      screen.unkey('s', saveGame)
       screen.unkey('r', resetGame)
       screen.unkey('return', splitGame)
     }
-  }, [])
+  }, [dispatch])
 }
 
 
